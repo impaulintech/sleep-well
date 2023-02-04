da92734e94984b048e2b93db01817d17245b4c30
John Paul Banera <johnpaul.banera@sun-asterisk.com>
2023-02-04 14:38:45 +0800

    [SWELL-552] - [BE] Create CRUD API for Pre Choices

--- api/app/Http/Requests/UpdatePreQuestionRequest.php
+++ api/app/Http/Requests/UpdatePreQuestionRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StorePreChoiceRequest extends FormRequest
+class UpdatePreQuestionRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,8 +24,8 @@ class StorePreChoiceRequest extends FormRequest
     public function rules()
     {
         return [
-            'pre_choice' => 'required|string',
-            'pre_question_id' => 'required|numeric'
+            'id' => 'numeric|string',
+            'pre_question' => 'required|string'
         ];
     }
 }
