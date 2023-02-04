1b0eb2640bcbddab20150aa390f4fcf83cd93e1d
John Paul Banera <johnpaul.banera@sun-asterisk.com>
2023-02-05 07:46:54 +0800

    [SWELL-300] - [BE] Create GET all needed data for the user Main Questions with Choices assessment page

--- api/routes/user/user.php
+++ api/routes/user/user.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Email\VerificationController;
 use App\Http\Controllers\Email\ForgotPasswordController;
+use App\Http\Controllers\MainQuestionController;
 use App\Http\Controllers\PreQuestionController;
 use App\Http\Controllers\UserController;
 
@@ -11,4 +12,5 @@ Route::group(['middleware' => 'auth:sanctum', 'prefix' => '/user'], function () 
     Route::put('/update', [UserController::class, 'update']);
 
     Route::get('/pre-questions', [PreQuestionController::class, 'assessment']);
+    Route::get('/main-questions', [MainQuestionController::class, 'assessment']);
 });
