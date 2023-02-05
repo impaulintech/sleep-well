008300a78083f38c2f97ad3dd419135819614280
John Paul Banera <93037350+impaulintech@users.noreply.github.com>
2023-02-05 20:36:44 +0800

    [SWELL-271] - [FE] Integrate API for Main Questions (#40)
    Co-authored-by: John Paul Banera <johnpaul.banera@sun-asterisk.com>

--- client/src/api/user/Assessment.tsx
+++ client/src/api/user/Assessment.tsx
@@ -1,8 +1,17 @@
 import { instance } from "../instance";
 
 const Assessment = {
-  preQuestions: () => { 
-    return instance.get('/user/pre-questions');
+  preQuestions: () => {
+    return instance.get("/user/pre-questions");
+  },
+  mainQuestions: (data: any) => {
+    return instance.post("/user/main-questions", data);
+  },
+  setRecommendations: (data: any) => {
+    return instance.post("/user/given-recommendations", data);
+  },
+  getRecommendations: () => {
+    return instance.get("/user/given-recommendations");
   },
 };
 export default Assessment;
--- client/src/pages/recommendations.tsx
+++ client/src/pages/recommendations.tsx
@@ -1,35 +1,17 @@
 import { NextPage } from "next";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 
 import Card from "~/components/atoms/Card";
+import { GlobalContext } from "~/context/GlobalContext";
 import Recommendation from "~/components/templates/Recommendation";
 
 const Recommendations: NextPage = (): JSX.Element => {
-  const recommendations = [
-    {
-      id: 1,
-      question: "question 1",
-      recommendation: "test 1test test test test test",
-    },
-    {
-      id: 2,
-      question: "question 2",
-      recommendation: "test 2test test test test test",
-    },
-    {
-      id: 3,
-      question: "question 3",
-      recommendation: "test 3test test test test test",
-    },
-    {
-      id: 4,
-      question: "question 4",
-      recommendation: "test 4test test test test test",
-    },
-  ];
+  const { recommendation } = useContext(GlobalContext) as any;
+  const [recommendations] = recommendation;
+  const { latest } = recommendations || [];
 
   const [currentPage, setCurrentPage] = useState(0);
-  const totalPage = recommendations.length;
+  const totalPage = latest && latest.length;
 
   const handleNext = () => {
     setCurrentPage(currentPage + 1);
@@ -43,27 +25,27 @@ const Recommendations: NextPage = (): JSX.Element => {
         callback={handleNext}
         imageHeight={300}
         imageWidth={250}
-        loaderUrl={"/"}
+        loaderUrl={"/checklist"}
         loaderMessage={"Finalizing and saving data..."}
         finalButtonMessage="Go to checklist"
       >
-        <div className="text-xl font-medium space-y-5">
+        <div className="text-xl font-medium space-y-5 mb-6">
           <section className="space-y-1">
             <h2>Question:</h2>
             <p className="text-base font-normal">
-              {recommendations[currentPage]?.question}
+              {latest && latest[currentPage]?.main_question}
             </p>
           </section>
-          <section>
-            <h2>
-              Your Answer:
-              <span className="text-base font-normal ml-2">YES</span>
-            </h2>
+          <section className="space-y-1">
+            <h2>Your Answer:</h2>
+            <p className="text-base font-normal">
+              {latest && latest[currentPage]?.main_choice}
+            </p>
           </section>
         </div>
         <Card
           title="Recommendation"
-          message={recommendations[currentPage]?.recommendation}
+          message={latest && latest[currentPage]?.recommendation}
         />
       </Recommendation>
     </>
